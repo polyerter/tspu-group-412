cb3f38d4f22906c21faeaa25d7de9e949982411b
Vyacheslav <polyerter@gmail.com>
2024-02-15 15:51:18 +0700

    add main file

---
+++ main.py
@@ -0,0 +1,46 @@
+# print('Hello', 'Word!')
+
+print('Hello', 'Word!') # print Hello Word!
+print('Hello', 'Word!') # print Hello Word!
+print('Hello', 'Word!') # print Hello Word!
+
+# print Hello Word!
+print(
+    'Hello',
+    'Word!'
+    )
+
+if 3 > 2:
+    print('Yes, 3 > 2')
+
+    if 4 > 3:
+        print('but 3 < 4')
+
+print('Hello, John!')
+
+# print Number of 0 to 10
+# for i in range(10):
+#     print(i)
+
+a = 1
+b = 2
+c = 5.6
+
+s = 'Hello, John!'
+
+print(a, b, c, s)
+
+a = 'aaaa'
+
+print('a:', type(a))
+print('b:', type(b))
+print('c:', type(c))
+print('s:', type(s))
+
+b = float(b)
+c = str(c)
+d = int(2.9999999999999999)
+
+print('b:', type(b), b)
+print('c:', type(c), c)
+print('d:', type(d), d)
